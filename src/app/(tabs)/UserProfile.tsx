811544eba8eaee391c9a20d59e8c61f883bdba62
BillyLangdown <billy.langdown01@gmail.com>
2024-02-19 07:24:49 -0500

    Homepage component created and NavBar edit

--- src/app/(tabs)/UserProfile.tsx
+++ src/app/(tabs)/UserProfile.tsx
@@ -2,15 +2,13 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/src/components/EditScreenInfo';
 import { Text, View } from '@/src/components/Themed';
-import Homepage from '@/src/components/Homepage';
 
-export default function TabOneScreen() {
+export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Homepage/>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>UserProfile</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
 }
---
+++ src/components/Homepage.tsx
@@ -0,0 +1,16 @@
+
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import Colors from "../constants/Colors";
+
+
+
+export default function Homepage(){
+    return(
+        <View>
+            <Text> CultureConnect </Text>
+            <Button title='List'></Button>
+            <Button title='Map'></Button>
+        
+        </View>
+    )
+}
