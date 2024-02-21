12c89c4cc39abd1b5ec5aab3c239e83afacfc483
BillyLangdown <billy.langdown01@gmail.com>
2024-02-21 11:50:18 -0500

    addition of base input boxes on the postEvent page including date and time picker

--- src/app/PostEvent.tsx
+++ src/app/PostEvent.tsx
@@ -1,11 +1,17 @@
+
+import { enGB, registerTranslation } from 'react-native-paper-dates'
+registerTranslation('en-GB', enGB)
 import { useState } from "react";
 import React from "react";
 import Spinner from "react-native-loading-spinner-overlay";
 import { supabase } from "@/config/initSupabase";
 import { Text, View } from "../components/Themed";
 import { StyleSheet, ScrollView } from "react-native";
-import { useForm } from "react-hook-form";
 import { Button } from "react-native-paper";
+import { TextInput } from "react-native-paper";
+import { DatePickerInput, TimePickerModal} from 'react-native-paper-dates';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 
 export default function PostEvent() {
   const [title, setTitle] = useState("");
@@ -16,18 +22,78 @@ export default function PostEvent() {
   const [image, setImage] = useState("");
   const [hostId, setHostId] = useState("");
   const [loading, setLoading] = useState(false);
+  const [inputDate, setInputDate] = React.useState(new Date())
+  const [visible, setVisible] = React.useState(false)
 
   supabase
     .from("events")
     .insert([{ title: title, location: location, date: date, host_id: hostId }])
     .select();
 
+  
+    const onDismiss = React.useCallback(() => {
+      setVisible(false)
+    }, [setVisible])
+  
+    const onConfirm = React.useCallback(
+      ({ hours, minutes }) => {
+        setVisible(false);
+        console.log({ hours, minutes });
+      },
+      [setVisible]
+    );
+
+
   return (
     <View style={styles.container}>
       <Spinner visible={loading} />
 
       <Text style={styles.header}>Host an Event!</Text>
-      
+
+      <Text style={styles.label} >Enter title</Text>
+      <TextInput
+        placeholder="Title"
+        value={title}
+        onChangeText={setTitle}
+        mode="outlined"
+      />
+
+      <TextInput
+        placeholder="City"
+        value={location}
+        onChangeText={setLocation}
+        mode="outlined"
+      />
+
+
+    <SafeAreaProvider>  
+    <View style={{justifyContent: 'center', flex: 1, alignItems: 'center', maxHeight: '30%',  backgroundColor: 'transparent'}}>
+       <DatePickerInput
+          withDateFormatInLabel={false}
+          presentationStyle='pageSheet'
+          locale="en-GB"
+          value={inputDate}
+          onChange={(d) => setInputDate(d)}
+          inputMode="start"
+          style={{width: 200}}
+          mode="outlined"
+        /> 
+        </View>
+      </SafeAreaProvider>
+      <SafeAreaProvider >
+      <View style={{justifyContent: 'center', flex: 1, alignItems: 'center', maxHeight: '25%'}}>
+        <Button onPress={() => setVisible(true)} uppercase={false} mode="outlined">
+          Pick time
+        </Button>
+        <TimePickerModal
+          visible={visible}
+          onDismiss={onDismiss}
+          onConfirm={onConfirm}
+          hours={12}
+          minutes={14}
+        />
+      </View>
+    </SafeAreaProvider>
     </View>
   );
 }
@@ -38,7 +104,7 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    paddingTop: 200,
+    paddingTop: 20,
     padding: 20,
     backgroundColor: "#151515",
   },
