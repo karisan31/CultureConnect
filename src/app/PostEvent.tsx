cb5f1e6b448fb5584f6055ee95331cc76991e912
tjejack <tjeidanjack@gmail.com>
2024-02-22 11:03:52 +0000

    working on time picker

--- src/app/PostEvent.tsx
+++ src/app/PostEvent.tsx
@@ -1,48 +1,33 @@
-
-import { enGB, registerTranslation } from 'react-native-paper-dates'
-registerTranslation('en-GB', enGB)
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import React from "react";
 import Spinner from "react-native-loading-spinner-overlay";
 import { supabase } from "@/config/initSupabase";
 import { Text, View } from "../components/Themed";
 import { StyleSheet, ScrollView } from "react-native";
-import { Button } from "react-native-paper";
-import { TextInput } from "react-native-paper";
-import { DatePickerInput, TimePickerModal} from 'react-native-paper-dates';
-import { SafeAreaProvider } from "react-native-safe-area-context";
-
+import { Button, TextInput } from "react-native-paper";
+import {
+  DatePickerInput,
+  enGB,
+  registerTranslation,
+} from "react-native-paper-dates";
+registerTranslation("en-GB", enGB);
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function PostEvent() {
   const [title, setTitle] = useState("");
   const [location, setLocation] = useState("");
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState<Date>(new Date);
   const [maxAttendees, setMaxAttendees] = useState("");
   const [description, setDescription] = useState("");
   const [image, setImage] = useState("");
   const [hostId, setHostId] = useState("");
   const [loading, setLoading] = useState(false);
-  const [inputDate, setInputDate] = React.useState(new Date())
-  const [visible, setVisible] = React.useState(false)
+  const [inputDate, setInputDate] = useState<Date | undefined>(undefined);
 
-  supabase
-    .from("events")
-    .insert([{ title: title, location: location, date: date, host_id: hostId }])
-    .select();
-
-  
-    const onDismiss = React.useCallback(() => {
-      setVisible(false)
-    }, [setVisible])
-  
-    const onConfirm = React.useCallback(
-      ({ hours, minutes }) => {
-        setVisible(false);
-        console.log({ hours, minutes });
-      },
-      [setVisible]
-    );
-
+  // supabase
+  //   .from("events")
+  //   .insert([{ title: title, location: location, date: date, host_id: hostId }])
+  //   .select();
 
   return (
     <View style={styles.container}>
@@ -50,7 +35,7 @@ export default function PostEvent() {
 
       <Text style={styles.header}>Host an Event!</Text>
 
-      <Text style={styles.label} >Enter title</Text>
+      <Text style={styles.label}>Enter title</Text>
       <TextInput
         placeholder="Title"
         value={title}
@@ -65,35 +50,21 @@ export default function PostEvent() {
         mode="outlined"
       />
 
-
-    <SafeAreaProvider>  
+      <SafeAreaProvider>  
     <View style={{justifyContent: 'center', flex: 1, alignItems: 'center', maxHeight: '30%',  backgroundColor: 'transparent'}}>
        <DatePickerInput
           withDateFormatInLabel={false}
           presentationStyle='pageSheet'
           locale="en-GB"
+          placeholder="DD/MM/YYYY"
           value={inputDate}
-          onChange={(d) => setInputDate(d)}
+          onChange={(newDate) => setInputDate(newDate)}
           inputMode="start"
           style={{width: 200}}
           mode="outlined"
         /> 
         </View>
       </SafeAreaProvider>
-      <SafeAreaProvider >
-      <View style={{justifyContent: 'center', flex: 1, alignItems: 'center', maxHeight: '25%'}}>
-        <Button onPress={() => setVisible(true)} uppercase={false} mode="outlined">
-          Pick time
-        </Button>
-        <TimePickerModal
-          visible={visible}
-          onDismiss={onDismiss}
-          onConfirm={onConfirm}
-          hours={12}
-          minutes={14}
-        />
-      </View>
-    </SafeAreaProvider>
     </View>
   );
 }
@@ -105,7 +76,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 20,
-    padding: 20,
+    padding: 20, 
     backgroundColor: "#151515",
   },
   header: {
