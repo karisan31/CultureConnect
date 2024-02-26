818e68ea26e984e4914f1bf9a6c22c9a57d13f29
tjejack <tjeidanjack@gmail.com>
2024-02-26 15:01:49 +0000

    funcional attendance and cancellation of attendance for events

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -4,6 +4,7 @@ import { Avatar, Button, Card, Text } from "react-native-paper";
 import { Link, router } from "expo-router";
 import { Image, StyleSheet } from "react-native";
 import RemoteImage from "./RemoteImage";
+import { supabase } from "@/config/initSupabase";
 
 export const defaultPartyImage =
   "https://img.freepik.com/free-vector/happy-friends-celebrating-event-together_74855-7482.jpg";
@@ -22,12 +23,60 @@ type EventProps = {
 
 export default function EventCard({ event }: EventProps) {
   const [isAttending, setIsAttending] = React.useState<boolean>(false);
+  const [attendError, setAttendError] = React.useState<boolean>(false);
+  const [currentUser, setCurrentUser] = React.useState<string | undefined>(
+    undefined
+  );
   const eventDate = new Date(event.date);
 
   function goToEventPage() {
     router.navigate(`/(tabs)/Home/${event.event_id}`);
   }
 
+  React.useEffect(() => {
+    supabase.auth
+      .getUser()
+      .then((user) => {
+        setCurrentUser(user.data.user?.id);
+        return supabase
+          .from("attendees")
+          .select("ticket_id")
+          .eq("user_id", user.data.user?.id)
+          .eq("event_id", event.event_id);
+      })
+      .then((attending) => {
+        if (attending.data?.length === 0 || attending.error) {
+          setIsAttending(false);
+        } else {
+          setIsAttending(true);
+        }
+      });
+  }, []);
+  async function attendingClick() {
+    setAttendError(false);
+    if (isAttending) {
+      setIsAttending(false);
+      const { error } = await supabase
+        .from("attendees")
+        .delete()
+        .eq("user_id", currentUser)
+        .eq("event_id", event.event_id);
+      if (error) {
+        setIsAttending(true);
+        setAttendError(true);
+      }
+    } else {
+      setIsAttending(true);
+      const { data, error } = await supabase
+        .from("attendees")
+        .insert([{ user_id: currentUser, event_id: event.event_id }])
+        .select();
+      if (error) {
+        setIsAttending(false);
+        setAttendError(true);
+      }
+    }
+  }
   const readableDate = eventDate.toLocaleString("en-US", {
     year: "numeric",
     month: "long",
@@ -69,7 +118,16 @@ export default function EventCard({ event }: EventProps) {
         />
         <Card.Actions>
           <Button onPress={goToEventPage}>More Info</Button>
-          {isAttending ? <Button style={{backgroundColor: "pink"}} onPress={()=>{setIsAttending(false)}}>Cancel</Button> : <Button onPress={()=>{setIsAttending(true)}}>Going!</Button>}
+          {isAttending ? (
+            <Button
+              style={{ backgroundColor: "pink" }}
+              onPress={attendingClick}
+            >
+              Cancel
+            </Button>
+          ) : (
+            <Button onPress={attendingClick}>Going!</Button>
+          )}
         </Card.Actions>
       </Card>
     </View>
