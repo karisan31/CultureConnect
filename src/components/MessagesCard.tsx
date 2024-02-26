7c3a6a04444b4f9d2b87cff9e92110325d1d1f89
Ward Sarkhan <143825089+WSarkhan@users.noreply.github.com>
2024-02-26 17:14:43 +0000

    added user profile picture, names, date and also fixed the issue with the input box

--- src/components/MessagesCard.tsx
+++ src/components/MessagesCard.tsx
@@ -1,27 +1,81 @@
 import { Card } from "react-native-paper";
-import { Text, View } from "./Themed";
+import { ScrollView, Text, View } from "./Themed";
 import { KeyboardAvoidingView, StyleSheet } from "react-native";
-import { useCurrentUser } from "../Utils/api";
+import { fetchDataAndSetProfileData, useCurrentUser } from "../Utils/api";
+import { useEffect, useState } from "react";
+import { supabase } from "@/config/initSupabase";
+import RemoteImage from "./RemoteImage";
+import { defaultProfileImage } from "../app/(tabs)/TabFour/UserProfile";
 interface Chat {
   id: number;
   content: string;
   author_id: string;
+  created_at: string;
 }
 interface MessagesCardProps {
   chat: Chat;
+}
+interface ProfileData {
+  avatar_url: string;
+  first_name: string;
+  second_name: string;
+  email: string;
+  bio: string;
 }
 
 export default function MessagesCard({ chat }: MessagesCardProps) {
   const currentUser = useCurrentUser();
   const myMessage = chat.author_id === currentUser?.id;
+  const [profileData, setProfileData] = useState<ProfileData | null>(null);
+  useEffect(() => {
+    async function fetchProfileData() {
+      try {
+        const { data, error } = await supabase
+          .from("profiles")
+          .select("*")
+          .eq("id", chat.author_id)
+          .single();
+
+        if (error) {
+          throw error;
+        }
+
+        if (data) {
+          setProfileData(data);
+        }
+      } catch (error) {
+        console.error("Error fetching profile data");
+      }
+    }
+
+    fetchProfileData();
+  }, []);
   return (
-    <View
-      style={[
-        styles.messageContainer,
-        myMessage ? styles.userMessageContainer : styles.otherMessageContainer,
-      ]}
-    >
-      <Text>{chat.content}</Text>
+    <View>
+      <View>
+        <RemoteImage
+          path={profileData?.avatar_url}
+          fallback={defaultProfileImage}
+          style={[styles.profileImage, myMessage ? styles.currentUser : null]}
+          bucket="avatars"
+        />
+        <Text style={[styles.user, myMessage ? styles.currentUser : null]}>
+          {profileData?.first_name} {profileData?.second_name}
+        </Text>
+      </View>
+      <View
+        style={[
+          styles.messageContainer,
+          myMessage
+            ? styles.userMessageContainer
+            : styles.otherMessageContainer,
+        ]}
+      >
+        <Text>{chat.content}</Text>
+        <Text style={styles.time}>
+          {new Date(chat.created_at).toLocaleString()}
+        </Text>
+      </View>
     </View>
   );
 }
@@ -32,12 +86,22 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     padding: 8,
   },
+  user: {
+    fontSize: 12,
+  },
   messageContainer: {
     padding: 10,
     marginTop: 10,
     marginHorizontal: 10,
     borderRadius: 10,
     maxWidth: "80%",
+  },
+  profileImage: {
+    width: 25,
+    height: 25,
+    alignSelf: "center",
+    borderRadius: 125,
+    marginBottom: 1,
   },
   messageInput: {
     flex: 1,
@@ -52,5 +116,13 @@ const styles = StyleSheet.create({
   },
   otherMessageContainer: {
     backgroundColor: "grey",
+  },
+  currentUser: {
+    alignSelf: "flex-end",
+  },
+  time: {
+    fontSize: 12,
+    color: "#777",
+    alignSelf: "flex-end",
   },
 });
