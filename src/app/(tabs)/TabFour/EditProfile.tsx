7df6c11373f2c38417ee1de48a762579569a1e7a
joravarsinghpunia <joravarsinghpunia91@gmail.co>
2024-02-28 15:54:37 +0000

    edit profile done, and swipe down refresh added to user profile page

--- src/app/(tabs)/TabFour/EditProfile.tsx
+++ src/app/(tabs)/TabFour/EditProfile.tsx
@@ -1,11 +1,13 @@
+import { useEffect, useState } from "react";
 import { supabase } from "@/config/initSupabase";
 import { Text, TextInput, View } from "@/src/components/Themed";
-import { Alert, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { Alert, Image, StyleSheet } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import * as FileSystem from "expo-file-system";
 import { randomUUID } from "expo-crypto";
 import { decode } from "base64-arraybuffer";
-import { useState } from "react";
+import { Button } from "react-native-paper";
+import { ActivityIndicator } from "react-native";
 
 interface ProfileUpdates {
   first_name?: string;
@@ -20,6 +22,37 @@ export default function EditProfile() {
   const [bio, setBio] = useState("");
   const [image, setImage] = useState<string | null>(null);
   const [updates, setUpdates] = useState<ProfileUpdates>({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchUserProfile() {
+      try {
+        const user = await supabase.auth.getUser();
+        if (user) {
+          const { data, error } = await supabase
+            .from("profiles")
+            .select("first_name, second_name, bio")
+            .eq("id", user.data.user?.id)
+            .single();
+
+          if (error) {
+            throw error;
+          }
+
+          if (data) {
+            setFirstName(data.first_name || "");
+            setSecondName(data.second_name || "");
+            setBio(data.bio || "");
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching user profile", error);
+        Alert.alert("Error", "Failed to fetch user profile");
+      }
+    }
+
+    fetchUserProfile();
+  }, []);
 
   const handleUpdateProfile = async () => {
     try {
@@ -99,6 +132,7 @@ export default function EditProfile() {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Edit Profile Details</Text>
       <Text style={styles.label}>First Name</Text>
       <TextInput
         style={styles.inputField}
@@ -112,25 +146,27 @@ export default function EditProfile() {
         onChangeText={setSecondName}
       />
       <Text style={styles.label}>Bio</Text>
-      <TextInput
-        style={styles.inputField}
-        multiline
-        numberOfLines={4}
-        value={bio}
-        onChangeText={setBio}
-      />
-      <TouchableOpacity style={styles.button} onPress={pickImage}>
-        <Text style={{ color: "#fff" }}>Choose Image</Text>
-      </TouchableOpacity>
-      {image && (
-        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-      )}
-      <TouchableOpacity style={styles.button} onPress={handleUpdateProfile}>
-        <Text style={{ color: "#fff" }}>Update Profile</Text>
-      </TouchableOpacity>
+      <TextInput style={styles.inputField} value={bio} onChangeText={setBio} />
+      <Button style={styles.button} onPress={pickImage} mode="contained">
+        Change profile picture
+      </Button>
+      <View style={styles.previewImage}>
+        {image && (
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+        )}
+      </View>
+      <Button
+        style={styles.button}
+        onPress={handleUpdateProfile}
+        mode="contained"
+        disabled={isLoading}
+      >
+        {isLoading ? <ActivityIndicator color="#fff" /> : "Update Profile"}
+      </Button>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   label: {
     color: "gray",
@@ -140,11 +176,19 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     padding: 20,
   },
-  header: {
+  previewImage: {
+    alignItems: "center",
+  },
+  title: {
     fontSize: 30,
-    textAlign: "center",
-    margin: 50,
-    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    marginBottom: 30,
+  },
+  separator: {
+    marginVertical: 10,
+    height: 1,
+    width: "80%",
   },
   inputField: {
     marginVertical: 4,
@@ -157,8 +201,5 @@ const styles = StyleSheet.create({
   button: {
     marginVertical: 15,
     alignItems: "center",
-    backgroundColor: "#2b825b",
-    padding: 12,
-    borderRadius: 4,
   },
 });
