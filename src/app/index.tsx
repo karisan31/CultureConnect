d9a8512928b073d2e6e3cc8912d419e97b4065d6
karisan31 <kmathan31@outlook.com>
2024-02-28 13:44:37 +0000

    styling sign in and create account pages

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -105,114 +105,138 @@ const Login = () => {
   };
 
   return signIn ? (
-    <ScrollView style={styles.container}>
+    <ScrollView contentContainerStyle={styles.scroll}>
       <Spinner visible={loading} />
-
-      <Text style={styles.header}>Culture Connect</Text>
-      <Text style={styles.label}>Email</Text>
-      <TextInput
-        autoCapitalize="none"
-        placeholder="Your email here..."
-        value={email}
-        onChangeText={setEmail}
-        style={styles.inputField}
-      />
-      <Text style={styles.label}>Password</Text>
-      <TextInput
-        autoCapitalize="none"
-        secureTextEntry={!showPassword}
-        value={password}
-        onChangeText={setPassword}
-        placeholder="Password here..."
-        style={styles.inputField}
-      />
-      <MaterialCommunityIcons
-        name={showPassword ? "eye-off" : "eye"}
-        size={24}
-        color="#aaa"
-        onPress={toggleShowPassword}
-      />
-      <TouchableOpacity onPress={onSignInPress} style={styles.button}>
-        <Text style={{ color: "#fff", fontSize: 20 }}>Sign in</Text>
-      </TouchableOpacity>
-      <Button onPress={onCreateAccount} title="Create Account"></Button>
+      <View>
+        <Image
+          source={require("../../assets/images/profileCover.png")}
+          style={styles.coverImageOne}
+        />
+        <Image
+          source={require("../../assets/images/profileCover.png")}
+          style={styles.coverImageTwo}
+        />
+        <Image
+          source={require("../../assets/images/profileCover.png")}
+          style={styles.coverImageThree}
+        />
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.header}>Culture Connect</Text>
+        <Text>Email</Text>
+        <TextInput
+          autoCapitalize="none"
+          placeholder="Your email here..."
+          value={email}
+          onChangeText={setEmail}
+          style={styles.inputField}
+        />
+        <Text>Password</Text>
+        <TextInput
+          autoCapitalize="none"
+          secureTextEntry={!showPassword}
+          value={password}
+          onChangeText={setPassword}
+          placeholder="Password here..."
+          style={styles.inputField}
+        />
+        <MaterialCommunityIcons
+          name={showPassword ? "eye-off" : "eye"}
+          size={24}
+          color="#aaa"
+          onPress={toggleShowPassword}
+        />
+        <TouchableOpacity onPress={onSignInPress} style={styles.button}>
+          <Text style={{ color: "#fff", fontSize: 20 }}>Sign in</Text>
+        </TouchableOpacity>
+        <Button onPress={onCreateAccount} title="Create Account"></Button>
+      </View>
     </ScrollView>
   ) : (
-    <ScrollView style={styles.container}>
+    <ScrollView style={styles.createContainer}>
       <Spinner visible={loading} />
-      <Image
-        source={
-          image
-            ? { uri: image }
-            : require("../../assets/images/defaultProfile.png")
-        }
-        style={styles.profileImage}
-      />
-      <Text onPress={pickImage} style={styles.imageUploadButton}>
-        Upload Profile Photo
-      </Text>
-      <Text style={styles.label}>First Name</Text>
-      <TextInput
-        value={firstName}
-        onChangeText={setFirstName}
-        placeholder="Input your name here..."
-        style={styles.inputField}
-      />
-      <Text style={styles.label}>Second Name</Text>
-      <TextInput
-        value={secondName}
-        onChangeText={setSecondName}
-        placeholder="Input your surname here..."
-        style={styles.inputField}
-      />
-      <Text style={styles.label}>Bio</Text>
-      <TextInput
-        value={bio}
-        onChangeText={setBio}
-        placeholder="Input your bio here..."
-        style={styles.inputField}
-      />
-      <Text style={styles.label}>Email</Text>
-      <TextInput
-        autoCapitalize="none"
-        value={email}
-        onChangeText={setEmail}
-        placeholder="jon@gmail.com"
-        style={styles.inputField}
-      />
+      <View>
+        <Image
+          source={
+            image
+              ? { uri: image }
+              : require("../../assets/images/defaultProfile.png")
+          }
+          style={styles.profileImage}
+        />
+        <Text onPress={pickImage} style={styles.imageUploadButton}>
+          Upload Profile Photo
+        </Text>
+        <Text style={styles.createLabel}>First Name</Text>
+        <TextInput
+          value={firstName}
+          onChangeText={setFirstName}
+          placeholder="Input your name here..."
+          style={styles.createInputField}
+        />
+        <Text style={styles.createLabel}>Second Name</Text>
+        <TextInput
+          value={secondName}
+          onChangeText={setSecondName}
+          placeholder="Input your surname here..."
+          style={styles.createInputField}
+        />
+        <Text style={styles.createLabel}>Bio</Text>
+        <TextInput
+          value={bio}
+          onChangeText={setBio}
+          placeholder="Input your bio here..."
+          style={styles.createInputField}
+        />
+        <Text style={styles.createLabel}>Email</Text>
+        <TextInput
+          autoCapitalize="none"
+          value={email}
+          onChangeText={setEmail}
+          placeholder="jon@gmail.com"
+          style={styles.createInputField}
+        />
 
-      <Text style={styles.label}>Password</Text>
-      <TextInput
-        autoCapitalize="none"
-        secureTextEntry={!showPassword}
-        value={password}
-        onChangeText={setPassword}
-        placeholder="Password here..."
-        style={styles.inputField}
-      />
-      <MaterialCommunityIcons
-        name={showPassword ? "eye-off" : "eye"}
-        size={24}
-        color="#aaa"
-        onPress={toggleShowPassword}
-      />
-      <TouchableOpacity onPress={onSignUpPress} style={styles.button}>
-        <Text style={{ color: "#fff" }}>Sign Up</Text>
-      </TouchableOpacity>
-      <Button onPress={onCreateAccount} title="Login"></Button>
+        <Text style={styles.createLabel}>Password</Text>
+        <TextInput
+          autoCapitalize="none"
+          secureTextEntry={!showPassword}
+          value={password}
+          onChangeText={setPassword}
+          placeholder="Password here..."
+          style={styles.createInputField}
+        />
+        <MaterialCommunityIcons
+          name={showPassword ? "eye-off" : "eye"}
+          size={24}
+          color="#aaa"
+          onPress={toggleShowPassword}
+        />
+        <TouchableOpacity onPress={onSignUpPress} style={styles.createButton}>
+          <Text style={{ color: "#fff" }}>Sign Up</Text>
+        </TouchableOpacity>
+        <Button onPress={onCreateAccount} title="Login"></Button>
+      </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  scroll: {
+    maxHeight: 300,
+  },
   label: {
-    color: "gray",
+    color: "white",
   },
   container: {
     flex: 1,
-    paddingBottom: 100,
-    paddingTop: 90,
+    top: 100,
+    backgroundColor: "rgba(255,255,255,0)",
     padding: 20,
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
   header: {
     fontSize: 30,
@@ -223,11 +247,45 @@ const styles = StyleSheet.create({
     marginVertical: 4,
     height: 50,
     borderWidth: 1,
-    borderColor: "#2b825b",
+    //borderColor: "#CCCCCC",
     borderRadius: 4,
     padding: 10,
   },
   button: {
+    marginVertical: 15,
+    alignItems: "center",
+    backgroundColor: "#2b825b",
+    padding: 12,
+    borderRadius: 4,
+  },
+  profileImage: {
+    width: 140,
+    height: 140,
+    alignSelf: "center",
+    borderRadius: 85,
+  },
+  createLabel: {
+    color: "gray",
+  },
+  createContainer: {
+    flex: 1,
+    top: 50,
+    backgroundColor: "rgba(255,255,255,0)",
+    padding: 20,
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  createInputField: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#2b825b",
+    borderRadius: 4,
+    padding: 10,
+  },
+  createButton: {
     marginVertical: 15,
     alignItems: "center",
     backgroundColor: "#2b825b",
@@ -239,11 +297,27 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     fontWeight: "bold",
   },
-  profileImage: {
-    width: 140,
-    height: 140,
-    alignSelf: "center",
-    borderRadius: 85,
+  coverImageOne: {
+    width: 700,
+    height: 500,
+    top: -350,
+    left: 150,
+    borderRadius: 280,
+  },
+  coverImageTwo: {
+    width: 200,
+    height: 200,
+    top: -260,
+    right: 80,
+    transform: [{ scaleX: -1 }],
+    borderRadius: 100,
+  },
+  coverImageThree: {
+    width: 700,
+    height: 500,
+    left: -50,
+    transform: [{ scaleX: -1 }, { rotate: "90deg" }],
+    borderRadius: 1000,
   },
 });
 
